2e01e3fc754e108155f381058c6d70ac0eaeaa53
irhoedin <irhoedin@gmail.com>
2016-02-11 10:52:22 +0900

    Add class "TheClass" to test2.py
    “TheClass” class handles visitor’s basic data

--- test2.py
+++ test2.py
@@ -2,3 +2,15 @@ data = [0,1,2]
 print data
 print data
 print data
+
+class TheClass(object):
+    def __init__(self, name, sex, age):
+        """
+        In initiation, basic data are stored in the instance.
+        name: Str. First name only.
+        sex: Int.
+        age: Int.
+        """
+        self.name = name
+        self.sex = sex
+        self.age = age
